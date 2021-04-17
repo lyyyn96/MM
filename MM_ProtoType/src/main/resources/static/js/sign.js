b7c0dd74c17bc38d97fb34c9a3ce1a3a60c5f027
MovieMentor <pktpkt8917@gmail.com>
2021-04-18 02:11:47 +0900

    FEAT : Add Session Login

--- MM_ProtoType/src/main/resources/static/js/sign.js
+++ MM_ProtoType/src/main/resources/static/js/sign.js
@@ -1,17 +1,19 @@
-$(document).ready(function(){// 로그인
+// 로그인
+$(document).ready(function(){ 
 	$("#signInBtn").click(function(){
 		
 		var id=$("#floatingInput").val();
 		var pw=$("#floatingPassword").val();
 		
 		$.post("signIn",
-				  {			   
-				    id:id,
-				    pw:pw
-				  },
-				  function(data){
-				  var obj=JSON.parse(data);			  
-			  	if(obj.name){
+			{			   
+				id:id,
+				pw:pw
+			},
+			function(data){
+				var obj=JSON.parse(data);			  
+			  	
+				if(obj.name){
 				 	alert(obj.name+ "님 환영합니다.");
 				 	location.href = "home";
 					$.cookie("logined",obj.name + "님 환영합니다.");	
@@ -19,6 +21,22 @@ $(document).ready(function(){// 로그인
 					alert(obj.msg);
 				}	
 			});
-			alert("b");
+		alert("무비 멘토 접속 중");
 	});
 });
+
+
+
+// 로그아웃
+$(document).on("click", "#logoutBtn", function(event) {  
+	$.post("#signOutBtn",
+		{
+		   
+		},
+		function(data, status){		  	
+			$.removeCookie("logined");
+			location.href = "index";		
+		}		   
+	);
+});
+
