b56fa27d5eba9e82973aff6f87c205eb50111cb4
MovieMentor <pktpkt8917@gmail.com>
2021-04-18 21:11:03 +0900

    STYLE : Add some Links

--- MM_ProtoType/bin/src/main/resources/static/js/sign.js
+++ MM_ProtoType/bin/src/main/resources/static/js/sign.js
@@ -1,24 +1,55 @@
-$(document).ready(function(){// 로그인
+// 로그인
+$(document).ready(function(){ 
 	$("#signInBtn").click(function(){
 		
 		var id=$("#floatingInput").val();
 		var pw=$("#floatingPassword").val();
 		
 		$.post("signIn",
-				  {			   
-				    id:id,
-				    pw:pw
-				  },
-				  function(data){
-				  var obj=JSON.parse(data);			  
-			  	if(obj.name){
+			{			   
+				id:id,
+				pw:pw
+			},
+			function(data){
+				var obj=JSON.parse(data);			  
+			  	
+				if(obj.name){
 				 	alert(obj.name+ "님 환영합니다.");
 				 	location.href = "home";
-					$.cookie("logined",obj.name + "님 환영합니다.");	
+					$.cookie("logined",obj.name + "님 환영합니다.", {path:'/'});	
 				}else{
 					alert(obj.msg);
 				}	
 			});
+		alert("무비 멘토 접속 중");
+	});
+});
+
+
+//로그인 쿠키 처리
+$(document).ready(function(){ 
+	$(function(){
+		var login=$.cookie('logined');
+		
+	$("#msgDiv").html(login);
+	});
+
+});
+
+
+// 로그아웃
+$(document).ready(function(){ 
+	$("#signOutBtn").click(function(){
+	$.post("signOut",
+		{
+		   
+		},
+		function(data){		  
+			alert(data);	
+			$.removeCookie("logined", {path:'/'});
+			location.href = "/";	
+			}		   
+		);
 	});
 });
 
--- MM_ProtoType/src/main/java/com/proto/mm/controller/MemberController.java
+++ MM_ProtoType/src/main/java/com/proto/mm/controller/MemberController.java
@@ -2,11 +2,16 @@ package com.proto.mm.controller;
 
 
 
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import com.proto.mm.service.MainService;
 import com.proto.mm.service.MemberService;
 
 @Controller
@@ -14,11 +19,18 @@ public class MemberController {
 
 	@Autowired
 	MemberService memberService;
-
-	@GetMapping("member/list")
-	public String member(Model model) {
+	
+	@Autowired
+	MainService mainService;
+	
+	
+	@GetMapping("memberList")
+	public String member(Model model,HttpServletRequest request,
+			HttpServletResponse response) {
+		// 세션을 체크해서 로그인 상태인지 확인
+		mainService.signInCheck(model, request, response);
 		memberService.showList(model);
-		return "member/list";
+		return "memberList";
 	}
 	
 	
