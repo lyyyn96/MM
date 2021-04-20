f9fdf72236f93c93460eeceb18045d81c10d6b7e
ChoiHyeRin <lyyyn@naver.com>
2021-04-20 22:55:55 +0900

    FEAT: Add movieList

---
+++ MM_ProtoType/src/main/java/com/proto/mm/model/Movie.java
@@ -0,0 +1,50 @@
+package com.proto.mm.model;
+
+import java.math.BigDecimal;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Movie {
+	@Id
+	@Column(name = "movie_code")
+	private BigDecimal movieCode;
+	
+	@NonNull
+	@Column(name = "movie_title")
+	private String movieTitle;
+	@NonNull
+	@Column(name = "movie_rating")
+	private BigDecimal movieRating;
+	@NonNull
+	@Column(name = "movie_genre")
+	private String movieGenre;
+	@NonNull
+	@Column(name = "movie_director")
+	private String movieDirector;
+	@NonNull
+	@Column(name = "movie_actor")
+	private String movieActor;
+	@NonNull
+	@Column(name = "movie_story")
+	private String movieStory;
+	@NonNull
+	@Column(name = "movie_rdate")
+	private String movieRdate;
+	@NonNull
+	@Column(name = "movie_rtime")
+	private String movieRtime;
+	@NonNull
+	@Column(name = "movie_price")
+	private BigDecimal moviePrice;
+}
---
+++ MM_ProtoType/src/main/java/com/proto/mm/repository/MovieRepository.java
@@ -0,0 +1,13 @@
+package com.proto.mm.repository;
+
+import java.math.BigDecimal;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.proto.mm.model.Movie;
+
+public interface MovieRepository extends JpaRepository<Movie, BigDecimal>{
+	
+	public Movie findByMovieTitle(String movieTitle);
+
+}
