29a9ced19e60390a1b192eb63e8094580ac36a63
MovieMentor <pktpkt8917@gmail.com>
2021-04-23 15:23:04 +0900

    STYLE : Fix typo

--- MM_ProtoType/src/main/java/com/proto/mm/model/Cart.java
+++ MM_ProtoType/src/main/java/com/proto/mm/model/Cart.java
@@ -19,14 +19,14 @@ import lombok.NonNull;
 @AllArgsConstructor
 @NoArgsConstructor
 @SequenceGenerator(
-		name="MEMBER_NO_SEQ_GENERATOR",
-		sequenceName = "MEMBER_NO_SEQ", // 매핑할 데이터 베이스 시퀀스 이름
+		name="CART_NO_SEQ_GENERATOR",
+		sequenceName = "CART_NO_SEQ", // 매핑할 데이터 베이스 시퀀스 이름
 		initialValue = 1,
 		allocationSize = 1)
 public class Cart {
 	@Id  // PK임을 명시
 	@GeneratedValue(strategy = GenerationType.SEQUENCE,
-	generator ="MEMBER_NO_SEQ_GENERATOR")
+	generator ="CART_NO_SEQ_GENERATOR")
 	// GenerationType은 Auto가 기본, 자동으로 해줌. 성능은 시퀀스가 가장 좋지만 많은 건을 처리 하고
 	// 자동증가 기본값으로 id 설정 시 IDENTITY가 적절함 
 	@Column(name="cart_count")
