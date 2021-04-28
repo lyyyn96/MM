55b3685276c078c884e22c772af8f77c29116390
ChoiHyeRin <lyyyn@naver.com>
2021-04-29 05:24:58 +0900

    FEAT: Add using enter key and remove previous value in chat_section

--- MM_ProtoType/src/main/resources/static/js/chat.js
+++ MM_ProtoType/src/main/resources/static/js/chat.js
@@ -1,4 +1,6 @@
-//검색 
+//검색
+var isEntered = false;
+
 $(document).ready(function(){ 
 	$("#btn-chat").click(function(){
 		
@@ -9,8 +11,37 @@ $(document).ready(function(){
 				
 				},function(data){		  
 					//location.href="home";	
+				}		   
+			);
+			
+			isEntered = true;
+			
+			if (isEntered) {
+			      isEntered = false;
+			      document.querySelector("#btn-input").value = '';
+			}
+	});
+	
+	$("#btn-input").keyup(function(e){
+		if(e.keyCode == 13){
+			var chat=$("#btn-input").val();
+
+			$.get("chat",{
+				chat:chat
+				
+				},function(data){		  
+					//location.href="home";	
 					}		   
 				);
+			
+			isEntered = true;
+			
+			if (isEntered) {
+			      isEntered = false;
+			      document.querySelector("#btn-input").value = '';
+			}
+		}
 	});
+
 });
 
